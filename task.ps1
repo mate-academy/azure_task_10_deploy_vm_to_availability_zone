919c0712671320e4c3598e8802d6b4a779fd69be
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-23 10:43:12 +0300

    returned creating public ips, resource group, nsg, virtual network, subnet and ssh key in task.ps1

--- task.ps1
+++ task.ps1
@@ -3,11 +3,43 @@ $resourceGroupName = "mate-azure-task-10"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
 $subnetName = "default"
+$vnetAddressPrefix = "10.0.0.0/16"
+$subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub"
 $vmName1 = "matebox1"
 $vmName2 = "matebox2"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
+$publicIpAddressName1 = "linuxboxpip1"
+$publicIpAddressName2 = "linuxboxpip2"
+$publicIpDnsLabel1 = "matebox-$((Get-Random).ToString().Substring(0, 6) )"
+$publicIpDnsLabel2 = "matebox-$((Get-Random).ToString().Substring(0, 6) )"
+
+
+Write-Host "Creating a resource group $resourceGroupName ..."
+New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+Write-Host "Creating a network security group $networkSecurityGroupName ..."
+$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow;
+$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
+New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
+
+$subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
+New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet
+
+New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
+
+# Take a note that in this task VMs are deployed without public IPs and you won't be able
+# to connect to them - that's on purpose! The "free" Public IP resource (Basic SKU,
+# dynamic IP allocation) can't be deployed to the availability zone, and therefore can't
+# be attached to the VM. Don't trust me - test it yourself!
+# If you want to get a VM with public IP deployed to the availability zone - you need to use
+# Standard public IP SKU (which you will need to pay for, it is not included in the free account)
+# and set same zone you would set on the VM, but this is not required in this task.
+Write-Host "Creating two public IPs ..."
+New-AzPublicIpAddress -Name $publicIpAddressName1 -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $publicIpDnsLabel1 -Zone "1"
+New-AzPublicIpAddress -Name $publicIpAddressName2 -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $publicIpDnsLabel2 -Zone "2"
 
 Write-Host "Creating VM 1 in zone 1 ..."
 New-AzVm `
@@ -20,7 +52,8 @@ New-AzVm `
 -VirtualNetworkName $virtualNetworkName `
 -SecurityGroupName $networkSecurityGroupName `
 -SshKeyName $sshKeyName `
--Zone "1"
+-Zone "1" `
+-PublicIpAddressName $publicIpAddressName1
 
 Write-Host "Creating VM 2 in zone 2 ..."
 New-AzVm `
@@ -33,4 +66,5 @@ New-AzVm `
 -VirtualNetworkName $virtualNetworkName `
 -SecurityGroupName $networkSecurityGroupName `
 -SshKeyName $sshKeyName `
--Zone "2"
+-Zone "2" `
+-PublicIpAddressName $publicIpAddressName2
