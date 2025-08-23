00535343660e32acf1ca56114d3ed962ea390b1c
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-23 10:12:52 +0300

    refactored task.ps1 to meet task requirements

--- task.ps1
+++ task.ps1
@@ -6,10 +6,16 @@ $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
-$vmName = "matebox"
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub"
+$vmName1 = "matebox1"
+$vmName2 = "matebox2"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
+$publicIpAddressName1 = "linuxboxpip1"
+$publicIpAddressName2 = "linuxboxpip2"
+$publicIpDnsLabel1 = "matebox-$((Get-Random).ToString().Substring(0, 6) )"
+$publicIpDnsLabel2 = "matebox-$((Get-Random).ToString().Substring(0, 6) )"
+
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -31,16 +37,34 @@ New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey 
 # If you want to get a VM with public IP deployed to the availability zone - you need to use 
 # Standard public IP SKU (which you will need to pay for, it is not included in the free account)
 # and set same zone you would set on the VM, but this is not required in this task. 
-# New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel "random32987"
+Write-Host "Creating two public IPs ..."
+New-AzPublicIpAddress -Name $publicIpAddressName1 -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $publicIpDnsLabel1 -Zone "1"
+New-AzPublicIpAddress -Name $publicIpAddressName2 -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $publicIpDnsLabel2 -Zone "2"
 
+Write-Host "Creating VM 1 in zone 1 ..."
 New-AzVm `
 -ResourceGroupName $resourceGroupName `
--Name $vmName `
+-Name $vmName1 `
 -Location $location `
 -image $vmImage `
 -size $vmSize `
 -SubnetName $subnetName `
 -VirtualNetworkName $virtualNetworkName `
 -SecurityGroupName $networkSecurityGroupName `
--SshKeyName $sshKeyName 
-# -PublicIpAddressName $publicIpAddressName
+-SshKeyName $sshKeyName `
+-Zone "1" `
+-PublicIpAddressName $publicIpAddressName1
+
+Write-Host "Creating VM 2 in zone 2 ..."
+New-AzVm `
+-ResourceGroupName $resourceGroupName `
+-Name $vmName2 `
+-Location $location `
+-image $vmImage `
+-size $vmSize `
+-SubnetName $subnetName `
+-VirtualNetworkName $virtualNetworkName `
+-SecurityGroupName $networkSecurityGroupName `
+-SshKeyName $sshKeyName `
+-Zone "2" `
+-PublicIpAddressName $publicIpAddressName2
