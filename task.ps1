105c76e88605a4398aef43c73929fe84e5ee7d40
Yaroslava <lanhraf.yaroslava@gmail.com>
2025-11-22 14:13:23 +0200

    deploy 2 vm

--- task.ps1
+++ task.ps1
@@ -1,46 +1,64 @@
-$location = "uksouth"
 $resourceGroupName = "mate-azure-task-10"
+$location = "centralus"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
 $subnetName = "default"
 $vnetAddressPrefix = "10.0.0.0/16"
 $subnetAddressPrefix = "10.0.0.0/24"
 $sshKeyName = "linuxboxsshkey"
-$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" 
-$vmName = "matebox"
+
+$sshKeyPublicKey = Get-Content "~/.ssh/id_rsa.pub" -Raw
+
+$vmUsername = "azureuser"
+
+$vmNameBase = "matebox"
 $vmImage = "Ubuntu2204"
+
 $vmSize = "Standard_B1s"
 
-Write-Host "Creating a resource group $resourceGroupName ..."
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+function Create-SecurityRule([string]$Name, [int]$Priority, [int]$Port, [string]$Direction) {
+    return New-AzNetworkSecurityRuleConfig -Name $Name `
+        -Description "$Name access rule" `
+        -Access Allow `
+        -Protocol Tcp `
+        -Direction $Direction `
+        -Priority $Priority `
+        -SourceAddressPrefix "*" `
+        -SourcePortRange "*" `
+        -DestinationAddressPrefix "*" `
+        -DestinationPortRange "$Port"
+}
 
-Write-Host "Creating a network security group $networkSecurityGroupName ..."
-$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow;
-$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
-New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
+New-AzResourceGroup -Name $resourceGroupName -Location $location -Force
 
-$subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
-New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet
+$nsgRuleSSH = Create-SecurityRule -Name SSH -Priority 1001 -Port 22 -Direction Inbound
+$nsgRuleHTTP = Create-SecurityRule -Name HTTP -Priority 1002 -Port 8080 -Direction Inbound
 
-New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
+$nsg = New-AzNetworkSecurityGroup -Name $networkSecurityGroupName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -SecurityRules $nsgRuleSSH, $nsgRuleHTTP -Force
 
-# Take a note that in this task VMs are deployed without public IPs and you won't be able
-# to connect to them - that's on purpose! The "free" Public IP resource (Basic SKU,
-# dynamic IP allocation) can't be deployed to the availability zone, and therefore can't 
-# be attached to the VM. Don't trust me - test it yourself! 
-# If you want to get a VM with public IP deployed to the availability zone - you need to use 
-# Standard public IP SKU (which you will need to pay for, it is not included in the free account)
-# and set same zone you would set on the VM, but this is not required in this task. 
-# New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel "random32987"
+$subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix -NetworkSecurityGroup $nsg
+$vnet = New-AzVirtualNetwork -Name $virtualNetworkName `
+    -ResourceGroupName $resourceGroupName `
+    -Location $location `
+    -AddressPrefix $vnetAddressPrefix `
+    -Subnet $subnet -Force
 
-New-AzVm `
--ResourceGroupName $resourceGroupName `
--Name $vmName `
--Location $location `
--image $vmImage `
--size $vmSize `
--SubnetName $subnetName `
--VirtualNetworkName $virtualNetworkName `
--SecurityGroupName $networkSecurityGroupName `
--SshKeyName $sshKeyName 
-# -PublicIpAddressName $publicIpAddressName
+$sshKey = New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
+
+foreach ($i in 1..2) {
+    New-AzVm `
+        -ResourceGroupName $resourceGroupName `
+        -Name "$vmNameBase$i" `
+        -Location $location `
+        -Image $vmImage `
+        -Size $vmSize `
+        -SubnetName $subnetName `
+        -VirtualNetworkName $virtualNetworkName `
+        -SecurityGroupName $networkSecurityGroupName `
+        -SshKeyName $sshKeyName `
+        -Zone "$i" `
+        -Credential (Get-Credential -UserName $vmUsername)
+}
