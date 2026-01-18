87dada24839918a54c22e73611802192fe13bebb
Vladyslav Supruniuk <vladsupruniuk@gmail.com>
2026-01-18 13:38:40 +0200

    Fix AI comments

--- task.ps1
+++ task.ps1
@@ -9,7 +9,7 @@ $sshKeyName = "linuxboxsshkey"
 $sshKeyPublicKey = Get-Content "~/.ssh/id_rsa_azure.pub"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
-$vmSize = "Standard_B2ats_v2"
+$vmSize = "Standard_B1s"
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
