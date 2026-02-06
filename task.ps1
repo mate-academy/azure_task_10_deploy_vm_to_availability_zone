30a04fee3776db9f9e5f57359406cb2c5527f9d8
Pavlus Chaban <pavluschaban@MacBook-Pro-Olena.local>
2026-02-06 03:16:16 +0200

    Solution

--- task.ps1
+++ task.ps1
@@ -9,7 +9,7 @@ $sshKeyName = "linuxboxsshkey"
 $sshKeyPublicKey = Get-Content "../id_rsa.pub"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
-$vmSize = "Standard_B2ats_v2"
+$vmSize = "Standard_B1s"
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
